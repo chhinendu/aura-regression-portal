7eb3445a5c2bcfadb29048984260e8c9fb64f9ca
Chhinendu Paul <cpaul@mediaocean.com>
2019-11-11 14:16:43 +0530

    Initial commit

---
+++ regression-portal-api/src/main/java/com/mediaocean/aura/regressionportalapi/RegressionPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.mediaocean.aura.regressionportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegressionPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegressionPortalApiApplication.class, args);
+	}
+
+}
